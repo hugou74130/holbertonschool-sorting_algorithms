e6c7468e53e7bf720d51fe8670d6f0e36179a3b6
Hugo <hugoramos1466@gmail.com>
2025-12-09 15:34:52 +0100

    update files

--- sort.h
+++ sort.h
@@ -1,3 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,3 +16,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/* Sorting functions */
+void bubble_sort(int *array, size_t size);
+
+/* Printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
