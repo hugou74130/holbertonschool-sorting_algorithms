b3ffacdcd46aaba937477cbc10eac8659c1bd167
Hugo <hugoramos1466@gmail.com>
2025-12-10 16:37:28 +0100

    0-bubble_sort.c, 0-O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,6 +5,7 @@
  * @array: Pointer to the array
  * @size: Size of the array
  *
+ * Description: Uses the Bubble sort algorithm. Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -18,7 +19,6 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,12 +26,10 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-
 				print_array(array, size);
 				swapped = 1;
 			}
 		}
-
 		/* Optimization: If no swap, array is sorted */
 		if (swapped == 0)
 			break;
